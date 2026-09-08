0d9621984bc93430543536bed6d7b07f8230e80d
Saroj Lamichhane <69lamichhanesaroj@gmail.com>
2026-09-08 20:06:38 +0200

    gitignore

--- server/API/Program.cs
+++ server/API/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpsgsql(Server=ep-crimson-surf-a5vn7wan-pooler.us-east-2.aws.neon.tech;DB=neondb;UID=neondb_owner;PWD=npg_p5IUBlhx7tnu;SslMode=require);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices]MyDbContext dbContext) =>
+{
+    var myFlower = new Flowersystem()
+    {
+        Title = "flowername",
+        Id = "flowerId"
+        Description = "isBigFlower"
+    }
+    dbContext.Flowersystem.add.myFlower;
+    dbContext.SaveChanges();
+    var objects = dbContext.Flowersystem.ToList();
+    return objects;
+    
+} );
 
 app.Run();
